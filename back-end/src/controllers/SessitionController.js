aa5438fe0de1fc3017f94756fe344423408f56d8
JaiDesk <49767189+mecanico92@users.noreply.github.com>
2021-03-13 10:49:22 -0300

    atualized

--- back-end/src/controllers/SessitionController.js
+++ back-end/src/controllers/SessitionController.js
@@ -4,11 +4,11 @@ const User = require('../models/User');
 module.exports = {
     async store(req, res){
         const {email} = req.body;
-        if (await User.findOne({email})){
-            return res.json({error: 'E-mail já cadastrado!'})
-        } else{
-            return res.json(await User.create({ email })) 
+        let user = await User.findOne({email});
+        if(!user){
+            const user = await User.create({ email });
         }
+        return res.json(user)
     }
 }
 
--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const multer = require('multer');
+
+const uploaConf = require('./config/upload');
 const SessionController = require('./controllers/SessitionController');
 const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
-
+const upload = multer(uploaConf);
 routes.post('/sessions', SessionController.store);
-routes.post('/spots', SpotController.store);
+routes.post('/spots',upload.single('thumbnail'), SpotController.store);
+routes.get('/spots', SpotController.index);
+routes.get('/dashboards', DashboardController.show);
+routes.post('/spots/:spot_id/bookings', BookingController.store)
 
 module.exports = routes;
